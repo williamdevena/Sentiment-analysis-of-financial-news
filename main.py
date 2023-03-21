523a4ca7fe76c1263e7ac8d6fdfd4dea1ec6dc67
williamdevena <william98wdv@gmail.com>
2023-03-21 22:38:56 +0000

    Added baseline VADER

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 from src import data_analysis, data_processing
 from utils import costants
@@ -6,11 +7,30 @@ from utils import costants
 
 def main():
     data = data_processing.read_ds()
-    print(data)
-    tot_words, counts_words = data_analysis.calculate_stats_words(data)
-    print(len(tot_words), counts_words[:10])
+    #print(data)
 
-    ## RED DATA
+    ## STATISTICS DATASET
+    # tot_words, counts_words = data_analysis.calculate_stats_words(data)
+    # print(len(tot_words), counts_words[:10])
+
+
+
+    sia = SentimentIntensityAnalyzer()
+    #res = {}
+    list_dict = []
+    pred = []
+    for i, text in enumerate(data['text']):
+        #print(i, text)
+        #text = row['text']
+        #myid = row['Id']
+        res = sia.polarity_scores(text)
+        res.pop('compound')
+        list_dict.append(res)
+        y_pred = max(res, key=lambda x: res[x])
+        #if y_pred==
+        pred.append(y_pred)
+
+    print(pred, list_dict)
 
 
 
